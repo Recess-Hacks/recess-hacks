0e6dea69fe881965877acbebeb8113106711bc7d
ImperialKoi <imperialkoi9@gmail.com>
2025-08-13 16:58:38 -0400

    changed google image hopefully

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,12 @@ export default function RootLayout({
                 <link rel="icon" type="image/svg+xml" href="https://recesshacks.org/logo.png" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
                 <title>RecessHacks</title>
+
+                <meta property="og:title" content="RecessHacks 2025" />
+                <meta property="og:description" content="RecessHacks 5.0 is our fifth ever iteration hackathon. We are a highschool hackathon open to all students from August 23-24. Hours of coding, mentorship & fun!" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content="/logo.png" />
+                
                 <script defer src="https://cloud.umami.is/script.js"
                     data-website-id="515f16a5-8036-482d-b535-7cdc2310d52b"></script>
             </head>
